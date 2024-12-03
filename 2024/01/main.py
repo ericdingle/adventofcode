b3f823213858bc14a6273b08ae87eb05110f4f76
Eric Dingle <eric.dingle@gmail.com>
2024-12-03 16:39:20 -0700

    Small refinement to day 1 after Pete's suggestions.

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -4,6 +4,6 @@ lhs, rhs = zip(*[map(int, line.split('   ')) for line in f])
 
 lhs, rhs = sorted(lhs), sorted(rhs)
 
-print(sum([abs(lhs[i] - rhs[i]) for i in range(len(lhs))]))
+print(sum(abs(l - r) for l, r in zip(lhs, rhs)))
 
-print(sum([i * rhs.count(i) for i in lhs]))
+print(sum(i * rhs.count(i) for i in lhs))
