f9be38147a2d9c23a9d8b9b0eaa8264885777178
Eric Dingle <eric.dingle@gmail.com>
2024-12-03 17:30:40 -0700

    Revert day 1 back to original solution.

--- 2024/01/main.py
+++ 2024/01/main.py
@@ -1,7 +1,9 @@
-lhs, rhs = zip(*[map(int, line.split()) for line in open('input.txt')])
+f = open('input.txt', 'r')
+
+lhs, rhs = zip(*[map(int, line.split('   ')) for line in f])
 
 lhs, rhs = sorted(lhs), sorted(rhs)
 
-print(sum(abs(l - r) for l, r in zip(lhs, rhs)))
+print(sum([abs(lhs[i] - rhs[i]) for i in range(len(lhs))]))
 
-print(sum(i * rhs.count(i) for i in lhs))
+print(sum([i * rhs.count(i) for i in lhs]))
