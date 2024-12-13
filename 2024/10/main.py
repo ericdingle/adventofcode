c5e6321982ec457b968da1fe493da5b176476989
Eric Dingle <eric.dingle@gmail.com>
2024-12-12 16:46:34 -0800

    Implementation of day 10 part 2.

--- 2024/10/main.py
+++ 2024/10/main.py
@@ -1,21 +1,27 @@
-data = open('input2.txt').read().splitlines()
+from time import time
+
+data = open('input.txt').read().splitlines()
 data = [*map(lambda row: [*map(int, list(row))], data)]
 width, height = len(data[0]), len(data)
 
-def score(r, c):
+def score(r, c, distinct):
   i = data[r][c]
   if i == 9:
-    return {(r, c)}
+    return {(r, c)} if distinct else {(r, c, time())}
 
   s = set()
   for r2, c2 in ((r+1, c), (r-1, c), (r, c+1), (r, c-1)):
     if 0 <= r2 < height and 0 <= c2 < width and data[r2][c2] == i + 1:
-      s.update(score(r2, c2))
+      s.update(score(r2, c2, distinct))
   return s
 
-s = 0
-for r, row in enumerate(data):
-  for c, i in enumerate(row):
-    if i == 0:
-      s += len(score(r, c))
-print(s)
+def run(distinct):
+  s = 0
+  for r, row in enumerate(data):
+    for c, i in enumerate(row):
+      if i == 0:
+        s += len(score(r, c, distinct))
+  return s
+
+print(run(True))
+print(run(False))
