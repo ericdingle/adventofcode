1d00f4eeec0e21bb8439ea89f804462d87458caf
Eric Dingle <eric.dingle@gmail.com>
2024-12-12 18:17:58 -0800

    Implementation of day 11 part 2.

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -1,18 +1,20 @@
+from functools import cache
+
 data = [*map(int, open('input.txt').read().split())]
 
-def run(data):
-  out = []
-  for i in data:
-    if i == 0:
-      out.append(1)
-    elif len(s := str(i)) % 2 == 0:
-      j = len(s) // 2
-      out.extend([int(s[:j]), int(s[j:])])
-    else:
-      out.append(i * 2024)
-  return out
+@cache
+def func(input, depth):
+  if depth == 0:
+    return 1
 
-for i in range(25):
-  data = run(data)
+  if input == 0:
+    return func(1, depth - 1)
+  elif len(s := str(input)) % 2 == 0:
+    j = len(s) // 2
+    return func(int(s[:j]), depth - 1) + func(int(s[j:]), depth - 1)
+  else:
+    return func(input * 2024, depth - 1)
 
-print(len(data))
+print(sum(func(i, 6) for i in data))
+print(sum(func(i, 25) for i in data))
+print(sum(func(i, 75) for i in data))
