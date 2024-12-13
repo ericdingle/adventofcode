ce8ef102b7315f10414280c38994c1a6c6f545cd
Eric Dingle <eric.dingle@gmail.com>
2024-12-12 16:17:55 -0800

    Implementation of day 8 part 2.

--- 2024/08/main.py
+++ 2024/08/main.py
@@ -6,6 +6,7 @@ sub = lambda a, b: (a[0] - b[0], a[1] - b[1])
 in_grid = lambda a, w, h: 0 <= a[0] < h and 0 <= a[1] < w
 
 data = open('input.txt').read().splitlines()
+width, height = len(data[0]), len(data)
 
 coords_map = defaultdict(set)
 for r in range(len(data)):
@@ -13,11 +14,24 @@ for r in range(len(data)):
     if ch != '.':
       coords_map[ch].add((r, c))
 
-nodes = set()
-for ch, coords  in coords_map.items():
-  for a, b in combinations(coords, 2):
-    dist = sub(a, b)
-    for coord in (add(a, dist), sub(b, dist)):
-      if in_grid(coord, len(data), len(data[0])):
-        nodes.add(coord)
-print(len(nodes))
+def get_num_nodes(harmonics):
+  nodes = set()
+  for ch, coords  in coords_map.items():
+    for a, b in combinations(coords, 2):
+      dist = sub(a, b)
+
+      candidates = [a, b] if harmonics else []
+      num_iters = min(height // abs(dist[0]), width // abs(dist[1])) if harmonics else 1
+
+      for candidate, func in ((a, add), (b, sub)):
+        for i in range(num_iters):
+          candidate = func(candidate, dist)
+          candidates.append(candidate)
+
+      for coord in candidates:
+        if in_grid(coord, width, height):
+          nodes.add(coord)
+  return len(nodes)
+
+print(get_num_nodes(False))
+print(get_num_nodes(True))
