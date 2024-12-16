a34d638cbc89a74884656cc4ce299e9509942593
Eric Dingle <eric.dingle@gmail.com>
2024-12-16 13:12:18 -0700

    Implementation of day 14 part 2.

--- 2024/14/main.py
+++ 2024/14/main.py
@@ -1,3 +1,4 @@
+from itertools import groupby
 import re
 
 pattern = re.compile(r'p=(-?\d+),(-?\d+) v=(-?\d+),(-?\d+)')
@@ -35,3 +36,27 @@ def get_factor(filename, width, height):
 
 print(get_factor('input.txt', 11, 7))
 #print(get_factor('input2.txt', 101, 103))
+
+def find_xmas(filename, width, height):
+  robots = get_robots(filename)
+
+  i = 0
+  while True:
+    i += 1
+
+    for robot in robots:
+      x, y, vx, vy = robot
+      x = (x + vx) % width
+      y = (y + vy) % height
+      robot[0], robot[1] = x, y
+
+    grid = get_grid(robots, width, height)
+
+    for row in grid:
+      for key, group in groupby(row):
+        if key != 0 and len(list(group)) > 20:
+          for row in grid:
+            print(''.join([*map(lambda x: ' ' if x == 0 else '*', row)]))
+          return i
+
+#print(find_xmas('input2.txt', 101, 103))
