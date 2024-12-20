12ee335870af1a4cf2507e456fb89597e8e0c017
Eric Dingle <eric.dingle@gmail.com>
2024-12-20 10:06:08 -0700

    Implementation of day 17 part 2.

--- 2024/17/main.py
+++ 2024/17/main.py
@@ -1,4 +1,4 @@
-def run(filename):
+def read_input(filename):
   a, b, c, _, program = open(filename).read().splitlines()
 
   a = int(a.split()[-1])
@@ -6,6 +6,9 @@ def run(filename):
   c = int(c.split()[-1])
   program = [*map(int, program.split()[1].split(','))]
 
+  return a, b, c, program
+
+def run(a, b, c, program):
   def combo(operand):
     if 0 <= operand <= 3:
       return operand
@@ -24,9 +27,7 @@ def run(filename):
     opcode, operand = program[pc:pc+2]
 
     if opcode == 0:  # adv
-      num = a
-      den = pow(2, combo(operand))
-      a = num // den
+      a = a >> combo(operand)
     elif opcode == 1:  # bxl
       b = b ^ operand
     elif opcode == 2:  # bst
@@ -38,20 +39,44 @@ def run(filename):
     elif opcode == 5:  # out
       out.append(combo(operand) % 8)
     elif opcode == 6:  # bdv
-      num = a
-      den = pow(2, combo(operand))
-      b = num // den
+      b = a >> combo(operand)
     elif opcode == 7:  # cdv
-      num = a
-      den = pow(2, combo(operand))
-      c = num // den
+      c = a >> combo(operand)
     else:
       raise Exception('Unknown opcode: %d' % opcode)
 
     if opcode != 3:  # jnz
       pc += 2
 
+  return out
+
+def part1(filename):
+  a, b, c, program = read_input(filename)
+  out = run(a, b, c, program)
   return ','.join([*map(str, out)])
 
-print(run('input.txt'))
-#print(run('input2.txt'))
+print(part1('input.txt'))
+#print(part1('input3.txt'))
+
+def part2(filename):
+  _, b, c, program = read_input(filename)
+  l = len(program)
+
+  def traverse(a, depth):
+    if depth == l:
+      return a
+
+    a = a << 3
+    for i in range(8):
+      out = run(a + i, b, c, program)
+      if out == program[l - depth - 1:]:
+        a2 = traverse(a + i, depth + 1)
+        if a2 != -1:
+          return a2
+
+    return -1
+
+  return traverse(0, 0)
+
+print(part2('input2.txt'))
+#print(part2('input3.txt'))
